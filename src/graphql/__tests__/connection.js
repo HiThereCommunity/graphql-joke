85e954b8a4e9b8e446cd82fe94cccf4825c5a13c
Dirk-Jan Rutten <dirkjanrutten@gmail.com>
2016-12-28 16:09:13 +0100

    Wrote tests for GraphQL schema

---
+++ src/graphql/__tests__/connection.js
@@ -0,0 +1,110 @@
+// @flow
+import { testDBConnection } from '../../testUtils'
+import {PostgresConnector} from '../../connectors'
+import { graphql } from 'graphql'
+
+import schema from '../'
+
+describe('GraphQL connections', () => {
+  const postgres = new PostgresConnector(testDBConnection, true)
+
+  const rootValue = {
+    db: postgres
+  }
+
+  beforeAll(async () => {
+    // clear the database before each test. Make sure that the name of the database table matches test.
+    await postgres.getSequelize().sync({force: true, match: /test$/})
+
+    const data = [
+      { id: 1, title: 'todo 1', completed: false },
+      { id: 2, title: 'todo 2', completed: true },
+      { id: 3, title: 'todo 3', completed: false },
+      { id: 4, title: 'todo 4', completed: false }
+    ]
+
+    await postgres.getTodoItemEntity().bulkCreate(data, { validate: true })
+  })
+
+  afterAll(() => postgres.getSequelize().close())
+
+  it('fetches all the todos in the list', async () => {
+    const query = `
+      query TodoListQuery {
+        todoList {
+          items {
+            ...TodoItemConnectionQuery
+          }
+          completed {
+            ...TodoItemConnectionQuery
+          }
+          active {
+            ...TodoItemConnectionQuery
+          }
+        }
+      }
+
+      fragment TodoItemConnectionQuery on TodoItemConnection {
+        edges {
+          cursor
+          node {
+            id
+            title
+            completed
+          }
+        }
+      }
+    `
+
+    const result = await graphql(schema, query, rootValue)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('fetches the first todo in the list after a cursor', async () => {
+    const query = `
+      query FirstAfterCursorTodoQuery {
+        todoList {
+          items(first: 1 after: "YXJyYXljb25uZWN0aW9uOjA=") {
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              startCursor
+              endCursor
+            }
+            edges {
+              node {
+                id
+                title
+                completed
+              }
+            }
+          }
+        }
+      }
+    `
+
+    const result = await graphql(schema, query, rootValue)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('fetches no todos at the end of the list', async () => {
+    const query = `
+      query EndOfTodoQuery {
+        todoList {
+          items(first: 1 after: "YXJyYXljb25uZWN0aW9uOjM=") {
+            edges {
+              node {
+                id
+                title
+                completed
+              }
+            }
+          }
+        }
+      }
+    `
+
+    const result = await graphql(schema, query, rootValue)
+    expect(result).toMatchSnapshot()
+  })
+})
---
+++ src/graphql/__tests__/objectIdentification.js
@@ -0,0 +1,70 @@
+// @flow
+import { testDBConnection } from '../../testUtils'
+import {PostgresConnector} from '../../connectors'
+import { graphql } from 'graphql'
+
+import schema from '../'
+
+describe('GraphQL object identification', () => {
+  const postgres = new PostgresConnector(testDBConnection, true)
+
+  const rootValue = {
+    db: postgres
+  }
+
+  beforeEach(async () => {
+    // clear the database before each test. Make sure that the name of the database table matches test.
+    await postgres.getSequelize().sync({force: true, match: /test$/})
+  })
+
+  afterAll(() => postgres.getSequelize().close())
+
+  it('fetches the ID of the todo list', async () => {
+    const query = `
+      query TodoListQuery {
+        todoList {
+          id
+        }
+      }
+    `
+
+    const result = await graphql(schema, query, rootValue)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('fetches the todo list node', async () => {
+    const query = `
+      query TodoListNode {
+        node(id: "VG9kb0xpc3Q6VG9kb0xpc3RJZA==") {
+          id
+        }
+      }
+    `
+
+    const result = await graphql(schema, query, rootValue)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('fetches the todo item node', async () => {
+    await postgres.getTodoItemEntity().create({
+      id: 1,
+      title: 'todo',
+      completed: false
+    })
+
+    const query = `
+      query TodoItemNode {
+        node(id: "VG9kb0l0ZW06MQ==") {
+          id
+          ...on TodoItem {
+            title
+            completed
+          }
+        }
+      }
+    `
+
+    const result = await graphql(schema, query, rootValue)
+    expect(result).toMatchSnapshot()
+  })
+})
